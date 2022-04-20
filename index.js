c23a84ea9c7b93a8ebf2a0d37fc216f785a5c742
omar <omar22dp@gmail.com>
2022-04-20 16:12:12 -0500

    creando proyecto y agregando dependencias

---
+++ index.js
@@ -0,0 +1,8 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander("Carlo")
+const omar = new MissionCommander("Omar")
+const ronaldo = new MissionCommander("Ronaldo")
+
+console.log(carlo.name)
+console.log(omar.name)
+console.log(ronaldo.name)
